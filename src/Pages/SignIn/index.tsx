40a6275a8ba66dc7f708d323dad5d41b7361ac84
saulo <saulo.velho@transfero.com>
2023-02-20 00:04:20 -0300

    get token

--- src/Pages/SignIn/index.tsx
+++ src/Pages/SignIn/index.tsx
@@ -18,8 +18,9 @@ function SignIn() {
     signInWithPopup(auth, provider)
       .then((result) => {
         // This gives you a Google Access Token. You can use it to access the Google API.
-        //const credential = GoogleAuthProvider.credentialFromResult(result);
-        //const token = credential?.accessToken;
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        console.log(token);
         // The signed-in user info.
         const user = result.user;
         console.log(user);
