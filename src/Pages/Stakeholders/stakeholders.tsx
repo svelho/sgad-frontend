f2f048a72d3543c5cfe1ae0b40f44d7cc667cf02
saulo <saulo.velho@transfero.com>
2023-02-22 19:11:46 -0300

    added header, main, footer

---
+++ src/Pages/Stakeholders/stakeholders.tsx
@@ -0,0 +1,17 @@
+import "./stakeholders.css";
+import Header from "../../Components/layout/header";
+import Main from "../../Components/layout/main";
+import Footer from "../../Components/layout/footer";
+import Cover from "../../Components/cover/cover";
+
+function Stakeholders() {
+  return (
+    <div>
+      <Header />
+
+      <Footer note="Footer Note" />
+    </div>
+  );
+}
+
+export default Stakeholders;
