cdb9d6f3d23d483deca23807aff2f22d08310c35
saulo <saulo.velho@transfero.com>
2023-03-03 02:47:51 -0300

    teste

--- src/Components/appbar/appbar.tsx
+++ src/Components/appbar/appbar.tsx
@@ -5,7 +5,6 @@ import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import green from "@material-ui/core/colors/green";
-import brown from "@material-ui/core/colors/purple";
 import { Avatar, Badge, Box, Button, Menu, MenuItem } from "@material-ui/core";
 import Credentials from "../../models/credentials";
 import { deepOrange } from "@material-ui/core/colors";
