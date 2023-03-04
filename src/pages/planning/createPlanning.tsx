694837f716e42cc154d20254c25973caf34f1d99
saulo <saulo.velho@transfero.com>
2023-03-04 01:54:13 -0300

    update createPlanning

--- src/pages/planning/createPlanning.tsx
+++ src/pages/planning/createPlanning.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { Button, TextareaAutosize } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import DatePicker from "react-date-picker";
 import PopperUnstyled from "@mui/base/PopperUnstyled";
 import { styled } from "@mui/system";
@@ -20,74 +20,119 @@ import {
   Stack,
 } from "@mui/material";
 import "./createPlanning.css";
-import { minWidth } from "@mui/system";
-import { UseAxiosGet, UseAxiosPost } from "../../hooks/axios";
+import { UseAxiosGetWithParameter, UseAxiosPost } from "../../hooks/axios";
 import Credentials from "../../models/credentials";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Loading from "../../Components/loading/loading";
-import Activities from "../activities/activities";
+import Activity from "../../models/activity";
+import Policy from "../../models/policy";
 
 export default function CreatePlanning() {
-  const [width, setWidth] = React.useState(window.innerWidth);
-  const breakpoint = 1100;
+  //#region properties
   const [_color, set_color] = React.useState("green");
-  let [title, setTitle] = useState("");
-  let [description, setDescription] = useState("");
-  let [level, setLevel] = useState(0);
-  let obj: any;
+  let [name, setName] = useState("");
+  let [activity, setActivity] = useState({} as Activity);
+  let [objective, setObjective] = useState("");
+  let [goal1, setGoal1] = useState("");
+  let [goal2, setGoal2] = useState("");
+  let [goal3, setGoal3] = useState("");
+  let [selectedPolicies, setSelectedPolicies] = useState([] as Policy[]);
+  let [selectedStakeholders, setSelectedStakeholders] = useState(
+    [] as Credentials[]
+  );
   let [success, setSuccess] = useState(true);
   const [dateValue, setDateValue] = useState(new Date());
-  let [riskActivityField, setRiskActivityField] = useState(true);
-  let [policiesField, setPoliciesField] = useState(true);
-  let [stakeholdersField, setStakeholdersField] = useState(true);
+  let [policiesField, setPoliciesField] = useState(false);
+  let [stakeholdersField, setStakeholdersField] = useState(false);
+  let [activities, setActivities] = useState([] as Activity[]);
+  let [policies, setPolicies] = useState([] as Policy[]);
+  let [stakeholders, setStakeholders] = useState([] as Credentials[]);
+  let [numbersStake, setNumbersStake] = useState([] as number[]);
+  let [numbersPoli, setNumbersPoli] = useState([] as number[]);
 
-  React.useEffect(() => {
-    window.addEventListener("resize", () => setWidth(window.innerWidth));
+  console.log(activity.id);
+  //#endregion
+
+  //#region userEffect
+  useEffect(() => {
+    UseAxiosGetWithParameter(
+      `${process.env.REACT_APP_BACKEND}/v1/activities`,
+      headers
+    ).then((data) => setActivities(data));
+
+    UseAxiosGetWithParameter(
+      `${process.env.REACT_APP_BACKEND}/v1/policies`,
+      headers
+    ).then((data) => setPolicies(data));
+
+    UseAxiosGetWithParameter(
+      `${process.env.REACT_APP_BACKEND}/v1/users`,
+      headers
+    ).then((data) => setStakeholders(data));
   }, []);
+  //#endregion
 
-  const handleChange = (event: SelectChangeEvent) => {
-    setLevel(parseInt(event.target.value));
+  //#region functions
+  const cred = localStorage.getItem("credentials");
+  const credential = JSON.parse(cred ?? "") as Credentials;
+
+  const headers = {
+    Authorization: `Bearer ${credential.token}`,
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
   };
 
+  const clearFields = () => {};
+
+  const handleChangeActivity = (event: SelectChangeEvent) => {
+    const value = event.target.value;
+    setActivity(activities.filter((x) => x.id === value)[0]);
+  };
+
+  const CustomMultiSelect = React.forwardRef(function CustomMultiSelect(
+    props: MultiSelectUnstyledProps<number>,
+    ref: React.ForwardedRef<any>
+  ) {
+    const slots: MultiSelectUnstyledProps<number>["slots"] = {
+      root: StyledButton,
+      listbox: StyledListbox,
+      popper: StyledPopper,
+      ...props.slots,
+    };
+
+    return <MultiSelectUnstyled {...props} ref={ref} slots={slots} />;
+  });
+
+  const handleChangePoli = (event: any) => {
+    setPoliciesField(false);
+    const selected = event.target.ariaSelected;
+    const id = event.target.id as string;
+    const index = parseInt(id.substring(id.lastIndexOf("-") + 1));
+    if (selected.toString() === "false") numbersPoli.push(index);
+    else numbersPoli.splice(numbersPoli.indexOf(index), 1);
+
+    setNumbersPoli(numbersPoli);
+  };
+
+  const handleChangeStake = (event: any) => {
+    setStakeholdersField(false);
+    const selected = event.target.ariaSelected;
+    const id = event.target.id as string;
+    const index = parseInt(id.substring(id.lastIndexOf("-") + 1));
+    if (selected.toString() === "false") numbersStake.push(index);
+    else numbersStake.splice(numbersStake.indexOf(index), 1);
+
+    setNumbersStake(numbersStake);
+  };
+
+  //#endregion
+
+  //#region designer
   const changeLabel = () => {
     set_color("#4caf50");
   };
   const changeLabelBlur = () => {
     set_color("gray");
-  };
-
-  const createPolicy = () => {
-    setSuccess(false);
-    const cred = localStorage.getItem("credentials");
-    const credential = JSON.parse(cred ?? "") as Credentials;
-
-    const headers = {
-      Authorization: `Bearer ${credential.token}`,
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json",
-    };
-
-    const payload = {
-      title: title,
-      description: description,
-      level: level,
-    };
-
-    const returned = UseAxiosPost(
-      `${process.env.REACT_APP_BACKEND}/v1/policy/create`,
-      payload,
-      headers
-    ).then((data) => {
-      setSuccess(data);
-      if (data) {
-        setTitle("");
-        setDescription("");
-        setLevel(0);
-        alert("Salvo com sucesso!");
-      } else {
-        alert("Erro ao tentar cadastrar política");
-      }
-    });
   };
 
   const green = {
@@ -160,20 +205,6 @@ export default function CreatePlanning() {
     z-index: 1;
   `;
 
-  const CustomMultiSelect = React.forwardRef(function CustomMultiSelect(
-    props: MultiSelectUnstyledProps<number>,
-    ref: React.ForwardedRef<any>
-  ) {
-    const slots: MultiSelectUnstyledProps<number>["slots"] = {
-      root: StyledButton,
-      listbox: StyledListbox,
-      popper: StyledPopper,
-      ...props.slots,
-    };
-
-    return <MultiSelectUnstyled {...props} ref={ref} slots={slots} />;
-  });
-
   const StyledButton = styled("button")(
     ({ theme }) => `
     font-family: IBM Plex Sans, sans-serif;
@@ -238,6 +269,54 @@ export default function CreatePlanning() {
     `
   );
 
+  //#endregion
+
+  //#region save
+  const createPlanning = () => {
+    setSuccess(false);
+
+    if (numbersPoli && numbersPoli.length === 0) {
+      setPoliciesField(true);
+      setSuccess(true);
+      return false;
+    }
+
+    if (numbersStake && numbersStake.length === 0) {
+      setStakeholdersField(true);
+      setSuccess(true);
+      return false;
+    }
+
+    const payload = {
+      name: name,
+    };
+
+    const cred = localStorage.getItem("credentials");
+    const credential = JSON.parse(cred ?? "") as Credentials;
+
+    const headers = {
+      Authorization: `Bearer ${credential.token}`,
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    };
+
+    const returned = UseAxiosPost(
+      `${process.env.REACT_APP_BACKEND}/v1/policy/create`,
+      payload,
+      headers
+    ).then((data) => {
+      setSuccess(data);
+      if (data) {
+        clearFields();
+        alert("Salvo com sucesso!");
+      } else {
+        alert("Erro ao tentar cadastrar política");
+      }
+    });
+  };
+  //#endregion
+
+  //#region Components
   if (!success) return <Loading />;
   else
     return (
@@ -248,7 +327,7 @@ export default function CreatePlanning() {
         }}
         autoComplete="off"
         onSubmit={(e) => {
-          //createActivity();
+          createPlanning();
           e.preventDefault();
         }}
       >
@@ -258,96 +337,94 @@ export default function CreatePlanning() {
             id="standard-basic"
             label="Nome do Plano de Ação"
             variant="standard"
-            onChange={(event) => setTitle(event.target.value)}
-            value={title}
+            onChange={(event) => setName(event.target.value)}
+            value={name}
           />
-          <InputLabel sx={{ color: "green" }}>
-            Selecione a atividade de risco abordada por esse planejamento.
-          </InputLabel>
+          <InputLabel sx={{ color: "green" }}>Atividade de Risco</InputLabel>
           <Select
             required
             labelId="demo-simple-select-label"
-            value={level.toString()}
-            label="Nível de Importância"
-            onChange={handleChange}
+            value={activity.id || ""}
+            onChange={handleChangeActivity}
             onFocus={changeLabel}
             onBlur={changeLabelBlur}
           >
-            <MenuItem value={25}>Necessário</MenuItem>
-            <MenuItem value={50}>Relevante</MenuItem>
-            <MenuItem value={75}>Muito Relevante</MenuItem>
-            <MenuItem value={100}>Imprescindível</MenuItem>
+            {activities != null &&
+              activities.map((x: Activity) => (
+                <MenuItem value={x.id}>{x.title}</MenuItem>
+              ))}
           </Select>
-          <div hidden={!riskActivityField} className="validationError">
-            Por favor selecione um nível de risco.
-          </div>
           <TextField
             required
             id="standard-basic"
             label="Objetivo"
             //variant=""
-            onChange={(event) => setTitle(event.target.value)}
-            value={title}
+            onChange={(event) => setObjective(event.target.value)}
+            value={objective}
           />
           <TextField
             required
             id="standard-basic"
             label="Meta 1"
             //variant=""
-            onChange={(event) => setTitle(event.target.value)}
-            value={title}
+            onChange={(event) => setGoal1(event.target.value)}
+            value={goal1}
           />
           <TextField
             required
             id="standard-basic"
             label="Meta 2"
             //variant=""
-            onChange={(event) => setTitle(event.target.value)}
-            value={title}
+            onChange={(event) => setGoal2(event.target.value)}
+            value={goal2}
           />
           <TextField
             required
             id="standard-basic"
             label="Meta 3"
             //variant=""
-            onChange={(event) => setTitle(event.target.value)}
-            value={title}
+            onChange={(event) => setGoal3(event.target.value)}
+            value={goal3}
           />
-          <InputLabel id="demo-simple-select-label" sx={{ color: _color }}>
-            Adicione as políticas ambientais ao planejamento.
+          <InputLabel id="" sx={{ color: "#4caf50" }}>
+            Políticas ambientais
           </InputLabel>
-          <CustomMultiSelect defaultValue={[10, 20]}>
-            <StyledOption value={10}>Ten</StyledOption>
-            <StyledOption value={20}>Twenty</StyledOption>
-            <StyledOption value={30}>Thirty</StyledOption>
-            <StyledOption value={40}>Forty</StyledOption>
-            <StyledOption value={50}>Fifty</StyledOption>
+          <CustomMultiSelect
+            defaultValue={numbersPoli}
+            onChange={handleChangePoli}
+          >
+            {policies != null &&
+              policies.map((x: Policy, index) => (
+                <StyledOption value={index}>{x.title}</StyledOption>
+              ))}
           </CustomMultiSelect>
           <div hidden={!policiesField} className="validationError">
-            Por favor selecione um nível de risco.
+            Por favor selecione pelo menos uma política.
           </div>
-          <InputLabel id="demo-simple-select-label" sx={{ color: _color }}>
-            Selecione os stakeholders responsáveis pela execução do
-            planejamento.
+          <InputLabel id="demo-simple-select-label" sx={{ color: "#4caf50" }}>
+            Stakeholders responsáveis pela execução do planejamento.
           </InputLabel>
-          <CustomMultiSelect defaultValue={[10, 20]}>
-            <StyledOption value={10}>Ten</StyledOption>
-            <StyledOption value={20}>Twenty</StyledOption>
-            <StyledOption value={30}>Thirty</StyledOption>
-            <StyledOption value={40}>Forty</StyledOption>
-            <StyledOption value={50}>Fifty</StyledOption>
+          <CustomMultiSelect
+            defaultValue={numbersStake}
+            onChange={handleChangeStake}
+          >
+            {stakeholders != null &&
+              stakeholders.map((x: Credentials, index) => (
+                <StyledOption value={index}>{x.name}</StyledOption>
+              ))}
           </CustomMultiSelect>
           <div hidden={!stakeholdersField} className="validationError">
-            Por favor selecione um nível de risco.
+            Por favor selecione pelo menos um stakeholder.
           </div>
           <InputLabel id="demo-simple-select-label" sx={{ color: _color }}>
             Selecione a data de Início do Plano
           </InputLabel>
-          <DatePicker onChange={setDateValue} value={dateValue} />
+          <DatePicker required onChange={setDateValue} value={dateValue} />
           <Button variant="outlined" type="submit">
             Salvar
           </Button>
         </Stack>
       </Box>
     );
+  //#endregion
 }
